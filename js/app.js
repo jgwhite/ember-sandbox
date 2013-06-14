ff568e46ff230378ee0d897ac7519006353a4364
Jamie White <jamie@jgwhite.co.uk>
2013-06-14 22:37:32 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+App = Ember.Application.create();
+
+App.EditorController = Ember.Controller.extend({
+  js: (
+    'App = Ember.Application.create();'
+  ),
+  html: (
+    '<script type="text/x-handlebars">\n' +
+    '  <h1>Hello, world!</h1>\n' +
+    '</script>'
+  )
+});
+
+App.ViewerController = Ember.Controller.extend({
+  needs: ['editor'],
+  jsBinding: 'controllers.editor.js',
+  htmlBinding: 'controllers.editor.html'
+});
+
+App.EditorView = Ember.View.extend({
+  classNames: ['pane global editor']
+});
+
+App.ViewerView = Ember.View.extend({
+  classNames: ['pane global viewer']
+});
+
+App.AceView = Ember.View.extend({
+  classNames: ['ace-view'],
+
+  didInsertElement: function() {
+    // setup ACE...
+    var editor = ace.edit(this.get('element')),
+        session = editor.getSession();
+
+    editor.setTheme('ace/theme/twilight');
+
+    session.setMode('ace/mode/' + this.get('mode'))
+    session.setUseSoftTabs(true);
+    session.setTabSize(2);
+
+    editor.setValue(this.get('value'));
+
+    editor.on('change', $.proxy(this, 'editorDidChange'));
+
+    this.set('editor', editor);
+  },
+
+  editorDidChange: function() {
+    var editor = this.get('editor');
+    this.set('value', editor.getValue());
+  },
+
+  willDestroyElement: function() {
+    // teardown ACE...
+  }
+});
+
+Ember.Handlebars.helper('ace', App.AceView);
+
+App.FrameView = Ember.View.extend({
+  tagName: 'iframe',
+  src: 'iframe.html',
+  classNames: ['pane frame'],
+  attributeBindings: ['src'],
+
+  contentDidChange: function() {
+    var el = this.get('element'),
+        contentWindow = el.contentWindow,
+        content = this.get('content');
+
+    Ember.run(function() { contentWindow.postMessage(content, '*') });
+  }.observes('content'),
+
+  content: function() {
+    return this.getProperties('html', 'js');
+  }.property('html', 'js'),
+
+  didInsertElement: function() {
+    Ember.run.later(this, function() {
+      this.notifyPropertyChange('content');
+    }, 500);
+  }
+});
+
+Ember.Handlebars.helper('frame', App.FrameView);
