14b10c2fca99162ed0827aa713389055e098bcc8
Jamie White <jamie@jgwhite.co.uk>
2013-06-28 19:01:26 +0100

    Update dependencies
    - Latest Ember build
    - Latest grunt-ember-templates
    - jQuery 2.0.2

--- js/templates.js
+++ js/templates.js
@@ -1,6 +1,6 @@
 Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashTypes, hashContexts, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -8,7 +8,7 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.outlet),stack1 ? stack1.call(depth0, "nav", options) : helperMissing.call(depth0, "outlet", "nav", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.outlet || depth0.outlet),stack1 ? stack1.call(depth0, "nav", options) : helperMissing.call(depth0, "outlet", "nav", options))));
   data.buffer.push("\n    </nav>\n  </header>\n  <section class=\"main\">\n    ");
   hashTypes = {};
   hashContexts = {};
@@ -17,15 +17,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.render),stack1 ? stack1.call(depth0, "viewer", options) : helperMissing.call(depth0, "render", "viewer", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.render || depth0.render),stack1 ? stack1.call(depth0, "viewer", options) : helperMissing.call(depth0, "render", "viewer", options))));
   data.buffer.push("\n  </section>\n</section>\n\n<footer>\n  <a href=\"http://github.com/jgwhite/ember-sandbox\">View the source on GitHub &rarr;</a>\n</footer>\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["gist"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
 
 
@@ -38,8 +38,8 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 });
 
 Ember.TEMPLATES["gist/css"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -49,15 +49,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("css"),
     'mode': ("css")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["gist/hbs"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -67,15 +67,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("hbs"),
     'mode': ("html")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["gist/js"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -85,15 +85,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("js"),
     'mode': ("javascript")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["gist/nav"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
 
 function program1(depth0,data) {
@@ -117,19 +117,19 @@ function program5(depth0,data) {
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "gist.js", options) : helperMissing.call(depth0, "linkTo", "gist.js", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "gist.js", options) : helperMissing.call(depth0, "linkTo", "gist.js", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "gist.hbs", options) : helperMissing.call(depth0, "linkTo", "gist.hbs", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "gist.hbs", options) : helperMissing.call(depth0, "linkTo", "gist.hbs", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "gist.css", options) : helperMissing.call(depth0, "linkTo", "gist.css", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "gist.css", options) : helperMissing.call(depth0, "linkTo", "gist.css", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   return buffer;
@@ -137,8 +137,8 @@ function program5(depth0,data) {
 });
 
 Ember.TEMPLATES["loading"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   
 
 
@@ -147,8 +147,8 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 });
 
 Ember.TEMPLATES["local"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
 
 
@@ -161,8 +161,8 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 });
 
 Ember.TEMPLATES["local/css"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -172,15 +172,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("css"),
     'mode': ("css")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["local/hbs"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -190,15 +190,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("hbs"),
     'mode': ("html")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["local/js"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -208,15 +208,15 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'valueBinding': ("js"),
     'mode': ("javascript")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.ace || depth0.ace),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "ace", options))));
   data.buffer.push("\n");
   return buffer;
   
 });
 
 Ember.TEMPLATES["local/nav"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
 
 function program1(depth0,data) {
@@ -240,19 +240,19 @@ function program5(depth0,data) {
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "local.js", options) : helperMissing.call(depth0, "linkTo", "local.js", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "local.js", options) : helperMissing.call(depth0, "linkTo", "local.js", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "local.hbs", options) : helperMissing.call(depth0, "linkTo", "local.hbs", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "local.hbs", options) : helperMissing.call(depth0, "linkTo", "local.hbs", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "local.css", options) : helperMissing.call(depth0, "linkTo", "local.css", options));
+  stack2 = ((stack1 = helpers.linkTo || depth0.linkTo),stack1 ? stack1.call(depth0, "local.css", options) : helperMissing.call(depth0, "linkTo", "local.css", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
   data.buffer.push("\n");
   return buffer;
@@ -260,8 +260,8 @@ function program5(depth0,data) {
 });
 
 Ember.TEMPLATES["viewer"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [3,'>= 1.0.0-rc.4'];
-helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, hashContexts, hashTypes, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
 
@@ -272,7 +272,7 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
     'hbsBinding': ("hbs"),
     'cssBinding': ("css")
   },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
-  data.buffer.push(escapeExpression(((stack1 = helpers.frame),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "frame", options))));
+  data.buffer.push(escapeExpression(((stack1 = helpers.frame || depth0.frame),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "frame", options))));
   data.buffer.push("\n");
   return buffer;
   
